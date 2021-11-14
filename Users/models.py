b6908959038903ea9fdcf9140bcf4984677a211e
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-14 14:27:27 +0100

    update

--- Users/models.py
+++ Users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class StylusUserManager(BaseUserManager):
+    pass
+
+
+class StylusUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField("Email", unique=True)
+    username = models.CharField(max_length=200, blank=True, unique=True)
+
+    USERNAME_FIELD = "email"
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    objects = StylusUserManager()
+
+    def __str__(self):
+        return self.email
