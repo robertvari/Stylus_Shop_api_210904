8f6200054d92d8dcdac7d41a1228d7301aa8eb12
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-14 16:50:10 +0100

    update

--- stylus_shop_api/urls.py
+++ stylus_shop_api/urls.py
@@ -1,26 +1,16 @@
-"""stylus_shop_api URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from allauth.account.views import confirm_email
+from rest_auth.views import PasswordResetConfirmView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('rest_auth.urls')),
-
+    path('api/auth/registration/account-confirm-email/', confirm_email, name="account_email_verification_sent"),
+    path('api/auth/registration/account-confirm-email/<key>/', confirm_email),
+    path('api/auth/registration/', include('rest_auth.registration.urls')),
+    path('password-reset-form/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
     path('api/', include("Shop.urls"))
 ]
