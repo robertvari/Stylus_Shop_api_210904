281e91e0af98ca4ae59c4450ffe8738af3330767
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-14 14:38:23 +0100

    update

--- Users/models.py
+++ Users/models.py
@@ -3,7 +3,27 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 
 class StylusUserManager(BaseUserManager):
-    pass
+    def create_user(self, email, password=None, superuser=False):
+        if not email:
+            raise ValueError("User must have an email.")
+
+        if not password:
+            raise ValueError("User must have a password.")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+
+        user.set_password(password)
+
+        if superuser:
+            user.is_staff = True
+            user.is_admin = True
+            user.is_superuser = True
+
+        user.save(using=self._db)
+
+        return user
 
 
 class StylusUser(AbstractBaseUser, PermissionsMixin):
