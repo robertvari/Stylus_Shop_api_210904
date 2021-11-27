afb89cc315749c1d4005464a92d04c66ceffa053
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-27 17:51:03 +0100

    update

--- Shop/models.py
+++ Shop/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+def get_category_dir_path(instance, filename):
+    return f"shop/categories/{slugify(instance.title)}/{filename}"
+
+
+def image_dir_path(instance, filename):
+    if instance.subcategory:
+        return f"shop/{slugify(instance.category.title)}/{slugify(instance.subcategory.title)}/{slugify(instance.title)}/{filename}"
+    else:
+        return f"shop/{slugify(instance.category.title)}/{slugify(instance.title)}/{filename}"
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to=get_category_dir_path)
+
+    def __str__(self):
+        return self.title
