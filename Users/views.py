78841aafcde554af6e4ac7a48fb4206e629c8157
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-27 15:46:47 +0100

    update

--- Users/views.py
+++ Users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class ProfileView(APIView):
+    def get(self, request):
+        return Response({"message": "User profile"})
+
+    def patch(self, request):
+        return Response({"message": "Update profile"})
--- stylus_shop_api/urls.py
+++ stylus_shop_api/urls.py
@@ -6,11 +6,13 @@ from rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/auth/', include('rest_auth.urls')),
     path('api/auth/registration/account-confirm-email/', confirm_email, name="account_email_verification_sent"),
     path('api/auth/registration/account-confirm-email/<key>/', confirm_email),
     path('api/auth/registration/', include('rest_auth.registration.urls')),
     path('password-reset-form/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 
-    path('api/', include("Shop.urls"))
+    path('api/', include("Shop.urls")),
+    path('api/users/', include("Users.urls"))
 ]
