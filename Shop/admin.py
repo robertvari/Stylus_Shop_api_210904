8b6c01882f71366215005194ab850de6b321d332
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-27 18:13:03 +0100

    update

--- Shop/admin.py
+++ Shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, SubCategory, ShopItem, Order, OrderItems
+
+
+class ShopItemAdmin(admin.ModelAdmin):
+    list_display = ["title", "in_stock", "price", "featured"]
+    list_editable = ["in_stock", "price", "featured"]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["email", "user", "phone"]
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["item", "quantity"]
+
+
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(ShopItem, ShopItemAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItems, OrderItemAdmin)
