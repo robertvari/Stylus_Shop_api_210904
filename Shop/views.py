8dcb4cf551853e2788afc17beefd5f4268a6cef2
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-28 14:30:48 +0100

    update

--- Shop/views.py
+++ Shop/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
+from .models import Category
+
 
 class SiteInfo(APIView):
     permission_classes = [AllowAny]
@@ -14,4 +16,27 @@ class CategoriesView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request):
-        return Response([])
+        categories = Category.objects.all()
+
+        result = []
+
+        site_url = request.build_absolute_uri("/")[:-1]
+
+        def get_subcategories(current_category):
+            result = [i.title for i in current_category.subcategory.all()]
+            if result:
+                return result
+
+            return None
+
+        for category in categories:
+            result.append(
+                {
+                    "id": category.id,
+                    "title": category.title,
+                    "image": f"{site_url}{category.image.url}",
+                    "items": get_subcategories(category)
+                }
+            )
+
+        return Response(result)
--- stylus_shop_api/urls.py
+++ stylus_shop_api/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from allauth.account.views import confirm_email
 from rest_auth.views import PasswordResetConfirmView
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -16,3 +18,6 @@ urlpatterns = [
     path('api/', include("Shop.urls")),
     path('api/users/', include("Users.urls"))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
