4834e388b0863358867a7325c359a84afa7226a6
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-28 17:52:49 +0100

    update

--- stylus_shop_api/settings.py
+++ stylus_shop_api/settings.py
@@ -122,12 +122,24 @@ WSGI_APPLICATION = 'stylus_shop_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "stylusshop",
+            "USER": "robert",
+            "PASSWORD": "testpas123",
+            "HOST": "localhost",
+            "PORT": ""
+        }
+    }
 
 
 # Password validation
