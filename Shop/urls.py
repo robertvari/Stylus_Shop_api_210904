24a7f0df90d8d16413c57622526f08afc670e144
Robert Vari <mail.pythonsuli@gmail.com>
2021-11-28 15:01:24 +0100

    update

--- Shop/urls.py
+++ Shop/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import SiteInfoView, CategoriesView
+from .views import SiteInfoView, CategoriesView, ShopItemsView
 
 urlpatterns = [
     path("site-info/", SiteInfoView.as_view()),
     path("categories/", CategoriesView.as_view()),
+    path("shop-items/", ShopItemsView.as_view()),
 ]
--- Shop/views.py
+++ Shop/views.py
@@ -2,7 +2,32 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 
-from .models import Category, SiteInfo
+from .models import Category, SiteInfo, ShopItem
+
+
+class ShopItemsView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        result = []
+
+        item_list = ShopItem.objects.all()
+        site_url = request.build_absolute_uri("/")[:-1]
+
+        for shop_item in item_list:
+            result.append({
+                "id": shop_item.id,
+                "featured": shop_item.featured,
+                "slug": shop_item.slug,
+                "category": shop_item.category.title,
+                "subcategory": shop_item.subcategory.title if shop_item.subcategory else None,
+                "title": shop_item.title,
+                "price": shop_item.price,
+                "image": f"{site_url}{shop_item.image1.url}",
+                "description": shop_item.description
+            })
+
+        return Response(result)
 
 
 class SiteInfoView(APIView):
